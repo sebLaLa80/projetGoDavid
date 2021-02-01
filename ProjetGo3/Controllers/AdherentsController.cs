4edd129962cf364100fedae56525135ab934c6fe
sebastien lapierre lachance <sebastien.lapierrel@gmail.com>
2021-02-01 18:13:23 -0500

    premier commit

---
+++ ProjetGo3/Controllers/AdherentsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjetGo3.Models;
+
+namespace ProjetGo3.Controllers
+{
+    public class AdherentsController : Controller
+    {
+        private DotNetAppSqlDb_projetGoEntities db = new DotNetAppSqlDb_projetGoEntities();
+
+        // GET: Adherents
+        public ActionResult Index()
+        {
+            return View(db.Adherents.ToList());
+        }
+
+        // GET: Adherents/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Adherent adherent = db.Adherents.Find(id);
+            if (adherent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adherent);
+        }
+
+        // GET: Adherents/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Adherents/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "codeAdherent,nomAdherent,prenomAdherent,emailAdherent,telephoneAdherent,adresseAdherent,dateAdhesionAdherent,statutAdhesionAdherent")] Adherent adherent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Adherents.Add(adherent);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(adherent);
+        }
+
+        // GET: Adherents/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Adherent adherent = db.Adherents.Find(id);
+            if (adherent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adherent);
+        }
+
+        // POST: Adherents/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "codeAdherent,nomAdherent,prenomAdherent,emailAdherent,telephoneAdherent,adresseAdherent,dateAdhesionAdherent,statutAdhesionAdherent")] Adherent adherent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(adherent).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(adherent);
+        }
+
+        // GET: Adherents/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Adherent adherent = db.Adherents.Find(id);
+            if (adherent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adherent);
+        }
+
+        // POST: Adherents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Adherent adherent = db.Adherents.Find(id);
+            db.Adherents.Remove(adherent);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProjetGo3/Controllers/ProjetsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjetGo3.Models;
+
+namespace ProjetGo3.Controllers
+{
+    public class ProjetsController : Controller
+    {
+        private DotNetAppSqlDb_projetGoEntities db = new DotNetAppSqlDb_projetGoEntities();
+
+        // GET: Projets
+        public ActionResult Index()
+        {
+            var projets = db.Projets.Include(p => p.Equipe);
+            return View(projets.ToList());
+        }
+
+        // GET: Projets/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Projet projet = db.Projets.Find(id);
+            if (projet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projet);
+        }
+
+        // GET: Projets/Create
+        public ActionResult Create()
+        {
+            ViewBag.codeProjet = new SelectList(db.Equipes, "codeProjet", "codeAdherent");
+            return View();
+        }
+
+        // POST: Projets/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "codeProjet,titreProjet,descriptionProjet,sommaireProjet,statutProjet,dateDebutEstimeeProjet,dateFinEstimeeProjet,echeancierProjet,budgetProjet,totalFondsCollectesProjet,totalDepensesProjet,dateDebutRelleProjet,dateFinRelleProjet,realisationCompleteesProjet,risquesProjet,etatAvancementProjet,codeAdherentProposeProjet,codeAdherentCoopteProjet")] Projet projet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Projets.Add(projet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.codeProjet = new SelectList(db.Equipes, "codeProjet", "codeAdherent", projet.codeProjet);
+            return View(projet);
+        }
+
+        // GET: Projets/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Projet projet = db.Projets.Find(id);
+            if (projet == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.codeProjet = new SelectList(db.Equipes, "codeProjet", "codeAdherent", projet.codeProjet);
+            return View(projet);
+        }
+
+        // POST: Projets/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "codeProjet,titreProjet,descriptionProjet,sommaireProjet,statutProjet,dateDebutEstimeeProjet,dateFinEstimeeProjet,echeancierProjet,budgetProjet,totalFondsCollectesProjet,totalDepensesProjet,dateDebutRelleProjet,dateFinRelleProjet,realisationCompleteesProjet,risquesProjet,etatAvancementProjet,codeAdherentProposeProjet,codeAdherentCoopteProjet")] Projet projet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(projet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.codeProjet = new SelectList(db.Equipes, "codeProjet", "codeAdherent", projet.codeProjet);
+            return View(projet);
+        }
+
+        // GET: Projets/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Projet projet = db.Projets.Find(id);
+            if (projet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projet);
+        }
+
+        // POST: Projets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Projet projet = db.Projets.Find(id);
+            db.Projets.Remove(projet);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
